ae5ef429fe5511010e111d6c336f4b4a1d749e2f
aubehub <albalopsanchez@gmail.com>
2020-07-09 14:39:34 -0300

    Updates

---
+++ precache-manifest.1c43ee67824e0ce56f1ce9de8a9b96ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93e3b53222bd59e953de77bbfecf503",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "89a8ba6f8957aee67171",
+    "url": "/rock-paper-scissors/static/css/main.ed2796e2.chunk.css"
+  },
+  {
+    "revision": "2f3c872f4a424b621cb0",
+    "url": "/rock-paper-scissors/static/js/2.99d160c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rock-paper-scissors/static/js/2.99d160c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a8ba6f8957aee67171",
+    "url": "/rock-paper-scissors/static/js/main.89871723.chunk.js"
+  },
+  {
+    "revision": "8926c2d699cc2a08de9a",
+    "url": "/rock-paper-scissors/static/js/runtime-main.340cd97a.js"
+  }
+]);
